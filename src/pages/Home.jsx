6eeda817e6ff0d3addbebb774aebfa687d287bdf
Haris Bhora <bhoraniyaharis963@gmail.com>
2025-07-17 12:24:29 +0530

    check upadet home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Header from '../components/Header'
-import BlogList from '../components/BlogList'
-import Newsletter from '../components/Newsletter'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import BlogList from "../components/BlogList";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <>
-      <Navbar />
-      <Header />
-      <BlogList />
-      <Newsletter />
-      <Footer />
-    </>
-  )
-}
+    <div className="px-4 md:px-8 lg:px-16">
+      <>
+        <Navbar />
+        <Header />
+        <BlogList />
+        <Newsletter />
+        <Footer />
+      </>
+    </div>
+  );
+};
 
-export default Home 
+export default Home;
